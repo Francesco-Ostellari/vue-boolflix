d11f76ee71a5e19793925d546603ca5c53b302de
Francesco-Ostellari <francescoostellari@gmail.com>
2022-01-19 11:26:16 +0100

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- key -->
+    <!-- 295649702f864d86aa6f022b7f8435fd -->
+
+    <!-- esempio link api -->
+    <!-- https://api.themoviedb.org/3/search/movie?api_key=295649702f864d86aa6f022b7f8435fd&query=ritorno+al+futuro -->
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
