10211925460e794f29fdab7580766a594b1e5952
Francesco-Ostellari <francescoostellari@gmail.com>
2022-01-20 16:57:44 +0100

    add-card-component

---
+++ src/components/Card.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <div class="layout-card">
+      <img :src="image" :alt="title">
+        <span>TITOLO: {{title}}</span>
+        <span>TITOLO ORIGINALE: {{original}}</span>
+        <span>LINGUA: <i :class="'flag flag-' + getFlags(lang)"></i></span>
+        <span class="voto">VOTO: {{vote}}</span>
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: [
+    'title',
+    'image',
+    'original',
+    'lang',
+    'vote',
+  ],
+  methods: {
+    getFlags(lang){
+      switch (lang){
+        case 'en':
+          return 'us';
+        case 'ko':
+          return 'kr';
+        case 'ja':
+          return 'jp';
+        case 'ur':
+          return 'pk';
+        case 'zh':
+          return 'cn';
+        case 'el':
+          return 'gr';
+        default:
+          return lang;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.card {
+  padding: 0.5em 1em;
+  width: 400px;
+  height: 630px;
+  .layout-card {
+    padding: 1em 1.5em;
+    height: 100%;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1.2em;
+    img {
+      height: 100%;
+      width: 100%;
+    }
+    span {
+      text-align: center;
+    }
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,34 +4,26 @@
       <div class="row">
         <div class="col">
           <h1>FILM</h1>
-          <ul v-if="inputSearch.movies.length > 0" class="ul-movies">
-            <li class="li-movies" v-for="(movie, index) in inputSearch.movies" :key="index+'movies'">
-              <div>
-                TITOLO: {{movie.title}} 
-              </div>
-              <div>
-                TITOLO ORIGINALE: {{movie.original_title}} 
-              </div>
-              <div>
-                LINGUA: <i :class="(movie.original_language == 'en') ? 'flag flag-us' : `flag flag-${ movie.original_language }`"></i>
-              </div>
-              <div>
-                VOTO: {{movie.vote_average}} 
-              </div>
-              <hr>
-            </li>
-          </ul>
+          <div v-if="inputSearch.movies.length > 0" class="card-container p-5">
+            <Card v-for="(movie, index) in inputSearch.movies" :key="index+'movies'"
+              :title="movie.title" 
+              :original="movie.original_title" 
+              :lang="movie.original_language"
+              :vote="movie.vote_average"
+              :image="`https://image.tmdb.org/t/p/w342${movie.poster_path}`">
+            </Card>
+          </div>
           <h2 v-else>Nessun film trovato</h2>
           <h1>SERIE TV</h1>
-          <ul class="ul-movies" v-if="inputSearch.series.length > 0">
-            <li class="li-movies" v-for="(serie, index) in inputSearch.series" :key="index+'series'">
-              <div>TITOLO: {{serie.name}}</div>
-              <div>TITOLO ORIGINALE: {{serie.original_name}}</div>
-              <div>LINGUA: <i :class="(serie.original_language == 'en') ? 'flag flag-us' : `flag flag-${serie.original_language}`"></i></div>
-              <div>VOTO: {{serie.vote_average}}</div>
-              <hr>
-            </li>
-          </ul>
+          <div class="card-container p-5" v-if="inputSearch.series.length > 0">
+            <Card v-for="(serie, index) in inputSearch.series" :key="index+'tv'"
+              :title="serie.name" 
+              :original="serie.original_name" 
+              :lang="serie.original_language"
+              :vote="serie.vote_average"
+              :image="`https://image.tmdb.org/t/p/w342${serie.poster_path}`">
+            </Card>
+          </div>
           <h2 v-else>Nessuna Serie TV trovata</h2>
         </div>
       </div>
@@ -41,6 +33,8 @@
 
 <script>
 
+import Card from './Card.vue'
+
 export default {
   name: "Main",
   props: ["inputSearch"], 
@@ -49,16 +43,22 @@ export default {
 
     }
       
-  }
+  },
+  components: {
+		Card,
+	}
 }
 </script>
 
 <style lang="scss">
-.ul-movies {
-  list-style-type: disc;
-  .li-movies {
-    display: block;
+  main {
+    background-color: #211F1F;
+    .card-container {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    h1, h2 {
+      color: white;
+    }
   }
-}
-
 </style>
